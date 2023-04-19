0e8e0b6fac2b34440ee54aa1c13381aa8564b98f
juan <juanarangozapata35@gmail.com>
2023-04-19 12:10:30 -0500

    Hicimos una modificacion

--- App.jsx
+++ App.jsx
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View>
       <Text>
-      HOLA ESTAMOS TRABAJANDO PARA MEJORAR, agregando a git hub
+      HOLA ESTAMOS TRABAJANDO PARA MEJORAR, agregando a git hub,
+      hemos modificado para hacer un commit en la rama 
 
       </Text>
     </View>
