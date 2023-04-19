d79c65331e7d3b666ef6eb0685ba6ec53c2332e6
juan <juanarangozapata35@gmail.com>
2023-04-19 12:00:30 -0500

    agreamos el primer commit de la app

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+      HOLA ESTAMOS TRABAJANDO PARA MEJORAR, agregando a git hub
+
+      </Text>
+    </View>
+  )
+}
