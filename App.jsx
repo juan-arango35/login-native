b857c864bff80dab7307ee266dc19f2d117b7e80
juan <juanarangozapata35@gmail.com>
2023-04-19 12:44:43 -0500

    Modificaciones css

--- App.jsx
+++ App.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+
+const styles= StyleSheet.create({
+  container: {
+
+    backgroundColor: "blue",
+    color: "white",
+    padding: 15
+
+  }
+})
 
 export default function App() {
   return (
-    <View>
-      <Text>
+
+    
+    <View style={styles.container} > 
+      <Text style={styles.container}  >
       HOLA ESTAMOS TRABAJANDO PARA MEJORAR, agregando a git hub,
       hemos modificado para hacer un commit en la rama 
 
